fda6ba86616d173dfb43c84c2e2b11eac099d85b
Shamus Hammons <jlhamm@acm.org>
2015-11-17 14:01:56 -0600

    Bump version #.

--- version.h
+++ version.h
@@ -13,6 +13,6 @@
 
 #define MAJOR   1		// Major version number
 #define MINOR   4		// Minor version number
-#define PATCH   4		// Patch release number
+#define PATCH   5		// Patch release number
 
 #endif // __VERSION_H__
