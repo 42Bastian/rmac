cdf0e4fdb91fc086ddadf096874e7c76eadc4b2d
Shamus Hammons <jlhamm@acm.org>
2020-01-17 14:16:03 -0600

    Version bump for last commits; now at v2.0.10.

--- version.h
+++ version.h
@@ -15,7 +15,7 @@
 
 #define MAJOR   2		// Major version number
 #define MINOR   0		// Minor version number
-#define PATCH   9		// Patch release number
+#define PATCH   10		// Patch release number
 
 #endif // __VERSION_H__
 
