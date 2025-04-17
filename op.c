b7840fc0538d8ee1ba52d484b3a7d05553288567
42Bastian <bs42@monlynx.de>
2025-04-17 10:58:48 +0200

    Jaguar OP: Fix XPOS masking

--- op.c
+++ op.c
@@ -184,7 +184,7 @@ static int HandleBitmap(void)
 
         if ( noYPOSby2 == 0 ) ypos *= 2;
 	uint64_t p1 = 0x00 | (ypos << 3) | (iheight << 14) | (linkAddr << 21) | (dataAddr << 40);
-	uint64_t p2 = xpos | (bpp << 12) | (pitch << 15) | (dwidth << 18) | (iwidth << 28) | (index << 38) | (flags << 45) | (firstpix << 49);
+	uint64_t p2 = (xpos & 0xfff) | (bpp << 12) | (pitch << 15) | (dwidth << 18) | (iwidth << 28) | (index << 38) | (flags << 45) | (firstpix << 49);
 
 	lastSloc = sloc;
 	lastObjType = 0;
@@ -309,7 +309,7 @@ static int HandleScaledBitmap(void)
 
         if ( noYPOSby2 == 0 ) ypos *= 2;
 	uint64_t p1 = 0x01 | (ypos  << 3) | (iheight << 14) | (linkAddr << 21) | (dataAddr << 40);
-	uint64_t p2 = xpos | (bpp << 12) | (pitch << 15) | (dwidth << 18) | (iwidth << 28) | (index << 38) | (flags << 45) | (firstpix << 49);
+	uint64_t p2 = (xpos & 0xfff) | (bpp << 12) | (pitch << 15) | (dwidth << 18) | (iwidth << 28) | (index << 38) | (flags << 45) | (firstpix << 49);
 	uint64_t p3 = (xscale & 0xFF) | (yscale & 0xFF) << 8 | (remainder & 0xFF) << 16;
 
 	lastSloc = sloc;
