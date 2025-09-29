279a313097722038060e29bb703df809da6ae5ee
42Bastian <bs42@monlynx.de>
2025-09-29 20:54:39 +0200

    merge main 2.4.7

--- dirent_lose.c
+++ dirent_lose.c
@@ -15,6 +15,7 @@
 
 #include "dirent_lose.h"
 
+#define VC_EXTRALEAN
 #define WIN32_LEAN_AND_MEAN
 #include <windows.h>
 
--- op.c
+++ op.c
@@ -37,7 +37,6 @@ static TOKEN fixupExpr[4] = { CONST, 0, 0, ENDEXPR };
 //static PTR fixupPtr = { .tk = (fixupExpr + 1) };		// C99 \o/
 static PTR fixupPtr = { (uint8_t *)(fixupExpr + 1) };	// meh, it works
 
-extern int noYPOSby2;
 //
 // The main Object Processor assembler. Basically just calls the sub functions
 // to generate the appropriate code.
@@ -135,7 +134,7 @@ static int HandleBitmap(void)
 
 	if (!(eattr & DEFINED))
 		AddFixup(FU_QUAD | FU_OBJDATA, sloc, exprbuf);
-	else if (eattr & TDB)
+	else if ((eattr & TDB) && !(eattr & (SDECLLIST | GLOBAL)))
 		MarkRelocatable(cursect, sloc, (eattr & TDB), MQUAD, NULL);
 
 	uint64_t dataAddr = eval & 0xFFFFF8;
@@ -182,8 +181,7 @@ static int HandleBitmap(void)
 
 	ErrorIfNotAtEOL();
 
-        if ( noYPOSby2 == 0 ) ypos *= 2;
-	uint64_t p1 = 0x00 | (ypos << 3) | (iheight << 14) | (linkAddr << 21) | (dataAddr << 40);
+	uint64_t p1 = 0x00 | ((ypos * 2) << 3) | (iheight << 14) | (linkAddr << 21) | (dataAddr << 40);
 	uint64_t p2 = (xpos & 0xfff) | (bpp << 12) | (pitch << 15) | (dwidth << 18) | (iwidth << 28) | (index << 38) | (flags << 45) | (firstpix << 49);
 
 	lastSloc = sloc;
@@ -307,8 +305,7 @@ static int HandleScaledBitmap(void)
 
 	ErrorIfNotAtEOL();
 
-        if ( noYPOSby2 == 0 ) ypos *= 2;
-	uint64_t p1 = 0x01 | (ypos  << 3) | (iheight << 14) | (linkAddr << 21) | (dataAddr << 40);
+	uint64_t p1 = 0x01 | ((ypos * 2) << 3) | (iheight << 14) | (linkAddr << 21) | (dataAddr << 40);
 	uint64_t p2 = (xpos & 0xfff) | (bpp << 12) | (pitch << 15) | (dwidth << 18) | (iwidth << 28) | (index << 38) | (flags << 45) | (firstpix << 49);
 	uint64_t p3 = (xscale & 0xFF) | (yscale & 0xFF) << 8 | (remainder & 0xFF) << 16;
 
@@ -403,8 +400,8 @@ static int HandleBranch(void)
 		AddFixup(FU_QUAD | FU_OBJLINK, sloc, exprbuf);
 
 	ErrorIfNotAtEOL();
-        if ( noYPOSby2 == 0 ) ypos *= 2;
-	uint64_t p1 = 0x03 | (cc << 14) | (ypos << 3) | ((eval & 0x3FFFF8) << 21);
+
+	uint64_t p1 = 0x03 | (cc << 14) | ((ypos * 2) << 3) | ((eval & 0x3FFFF8) << 21);
 
 	lastObjType = 3;
 	D_quad(p1);
